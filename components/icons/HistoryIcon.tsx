9544139b2c35a35fe3e3aa8f271ba5f64dc5512b
Kuek Hao Yang <143677684+KuekHaoYang@users.noreply.github.com>
2025-06-08 14:57:01 +0800

    Update HistoryIcon.tsx

--- components/icons/HistoryIcon.tsx
+++ components/icons/HistoryIcon.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 interface IconProps extends React.SVGProps<SVGSVGElement> {}
@@ -6,18 +5,12 @@ interface IconProps extends React.SVGProps<SVGSVGElement> {}
 export const HistoryIcon: React.FC<IconProps> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 -960 960 960"
     width="24"
     height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    fill="currentColor"
     {...props}
   >
-    <path d="M12 8v4l3 3" />
-    <path d="M12 21a9 9 0 1 0-9-9" />
-    <path d="M19.34 14.66A9 9 0 1 0 12 21" /> {/* To complete the circle visually when stroke is thick */}
+    <path d="M480-120q-138 0-240.5-91.5T122-440h82q14 104 92.5 172T480-200q117 0 198.5-81.5T760-480q0-117-81.5-198.5T480-760q-69 0-129 32t-101 88h110v80H120v-240h80v94q51-64 124.5-99T480-840q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-480q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-120Zm112-192L440-464v-216h80v184l128 128-56 56Z"/>
   </svg>
 );
